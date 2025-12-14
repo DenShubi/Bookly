9467ab2cbf5f617845a4a16adef099c2dcdfee47
DenShubi <denindramigurd@student.ub.ac.id>
2025-12-14 17:24:57 +0700

    Update SupabaseClientProvider.kt

--- app/src/main/java/com/example/bookly/supabase/SupabaseClientProvider.kt
+++ app/src/main/java/com/example/bookly/supabase/SupabaseClientProvider.kt
@@ -1,9 +1,23 @@
 package com.example.bookly.supabase
 
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
+
 object SupabaseClientProvider {
     // Supabase configuration
     const val SUPABASE_URL = "https://jxxjdagjxinlwxnmagrd.supabase.co"
     const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4eGpkYWdqeGlubHd4bm1hZ3JkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NTY2NzQsImV4cCI6MjA4MDMzMjY3NH0.0GjbBHPRpEPaWjQI8sVI_IJwymoFf05iotE73UALX7A"
+
+    val client = createSupabaseClient(
+        supabaseUrl = SUPABASE_URL,
+        supabaseKey = SUPABASE_ANON_KEY
+    ) {
+        install(Auth)      // Untuk Login/User
+        install(Postgrest) // Untuk Database (Tabel reviews, books)
+        install(Storage)   // Untuk Upload Foto (Bucket)
+    }
 
     // Holds the current user's access token after login/signup
     @Volatile
